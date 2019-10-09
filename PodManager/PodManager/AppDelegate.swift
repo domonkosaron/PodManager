91d4c87534dcfb5ef8d2b3411a55fe721942fac8
Domonkos Áron <domonkos.aron@autsoft.hu>
2019-10-09 11:05:55 +0200

    Adds base Xcode project to repo

---
+++ PodManager/PodManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PodManager
+//
+//  Created by Domonkos Aron on 2019. 10. 09..
+//  Copyright © 2019. Domonkos Aron. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
