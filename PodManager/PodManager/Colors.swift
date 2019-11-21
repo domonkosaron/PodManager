c5ef9899a928c0180b09554f691dbe96cf27d4e5
Domonkos Áron <domonkos.aron@autsoft.hu>
2019-11-21 13:24:07 +0100

    Adds LeftMenu

---
+++ PodManager/PodManager/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  PodManager
+//
+//  Created by Domonkos Aron on 2019. 11. 21..
+//  Copyright © 2019. Domonkos Aron. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+public class Colors {
+    static let primaryColor = NSColor(calibratedRed: 69/255, green: 117/255, blue: 130/255, alpha: 1)
+    static let borderColor = NSColor(calibratedRed: 112/255, green: 112/255, blue: 112/255, alpha: 1)
+    static let secondaryColor = NSColor(calibratedRed: 242/255, green: 240/255, blue: 240/255, alpha: 1)
+    static let flatGreenColor = NSColor(calibratedRed: 124/255, green: 160/255, blue: 158/255, alpha: 1)
+    static let lightGray = NSColor(calibratedRed: 170/255, green: 170/255, blue: 170/255, alpha: 1)
+}
--- PodManager/PodManager/MainViewController.swift
+++ PodManager/PodManager/MainViewController.swift
@@ -22,8 +22,18 @@ class MainViewController: NSViewController {
     @IBOutlet weak var buttonsView: NSView!
     @IBOutlet weak var topBarView: NSView!
     @IBOutlet weak var searchBarView: NSView!
-    
-    var primaryColor = NSColor(calibratedRed: 69/255, green: 117/255, blue: 130/255, alpha: 1)
+    @IBOutlet weak var sideMenuView: NSView!
+    @IBOutlet weak var sideMenuBottomView: NSView!
+    @IBOutlet weak var gitHooksButton: NSButton!
+    @IBOutlet weak var showInFinderButton: NSButton!
+    @IBOutlet weak var terminalButton: NSButton!
+    @IBOutlet weak var settingsButton: NSButton!
+    @IBOutlet weak var separator1: NSView!
+    @IBOutlet weak var myPodsButton: NSButton!
+    @IBOutlet weak var popularButton: NSButton!
+    @IBOutlet weak var favouritesButton: NSButton!
+    @IBOutlet weak var templatesButton: NSButton!
+    @IBOutlet weak var separator2: NSView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,23 +41,47 @@ class MainViewController: NSViewController {
         configureProjectNameTextField()
         configureSearchView()
         configureButtonsView()
+        configureSideMenuView()
     }
     
     func configureProjectNameTextField() {
-        projectNameView.customBackgroundColor = primaryColor
+        projectNameView.customBackgroundColor = Colors.primaryColor
         projectNameTextField.drawsBackground = true
-        projectNameTextField.backgroundColor = primaryColor
+        projectNameTextField.backgroundColor = Colors.primaryColor
 //        projectNameTextField.layer?.cornerRadius = 25
     }
     
     func configureSearchView() {
-        searchView.customBackgroundColor = primaryColor
+        searchView.customBackgroundColor = Colors.primaryColor
         searchBarView.customBackgroundColor = .labelColor
         searchBarView.layer?.cornerRadius = 18
         searchBarTextField.focusRingType = .none
+        searchBarTextField.isBordered = false
+        searchBarTextField.backgroundColor = .clear
     }
     
     func configureButtonsView() {
-        buttonsView.customBackgroundColor = primaryColor
+        buttonsView.customBackgroundColor = Colors.primaryColor
+    }
+    
+    func configureSideMenuView() {
+        sideMenuView.customBackgroundColor = Colors.secondaryColor
+        separator1.customBackgroundColor = Colors.borderColor
+        separator2.customBackgroundColor = Colors.borderColor
+        
+        let pstyle = NSMutableParagraphStyle()
+        pstyle.alignment = .left
+        let leftMenuBottomAttributes = [ NSAttributedString.Key.foregroundColor : Colors.flatGreenColor, NSAttributedString.Key.paragraphStyle : pstyle ]
+
+        gitHooksButton.attributedTitle = NSAttributedString(string: "Git Hooks", attributes: leftMenuBottomAttributes)
+        showInFinderButton.attributedTitle = NSAttributedString(string: "Show in Finder", attributes: leftMenuBottomAttributes)
+        terminalButton.attributedTitle = NSAttributedString(string: "Terminal", attributes: leftMenuBottomAttributes)
+        settingsButton.attributedTitle = NSAttributedString(string: "Settings", attributes: leftMenuBottomAttributes)
+        
+        let leftMenuTopAttributes = [ NSAttributedString.Key.foregroundColor : Colors.lightGray, NSAttributedString.Key.paragraphStyle : pstyle ]
+        myPodsButton.attributedTitle = NSAttributedString(string: "MY PODS", attributes: leftMenuTopAttributes)
+        popularButton.attributedTitle = NSAttributedString(string: "􀙭  POPULAR", attributes: leftMenuTopAttributes)
+        favouritesButton.attributedTitle = NSAttributedString(string: "􀋃 FAVOURITES", attributes: leftMenuTopAttributes)
+        templatesButton.attributedTitle = NSAttributedString(string: "􀉆  TEMPLATES", attributes: leftMenuTopAttributes)
     }
 }
